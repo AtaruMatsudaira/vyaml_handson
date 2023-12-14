d8e13dff83abcdc87113fd9ebfa5a4a1546e61c0
mattun(thinkPad) <matsu222212@gmail.com>
2023-12-14 10:40:04 +0900

    pure C# での analyzerを実装

--- vyaml_analyzer/Program.cs
+++ vyaml_analyzer/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Zx;
+using static Zx.Env;
+using VYaml.Serialization;
+
+await ConsoleApp.RunAsync(args, MainAsync);
+
+async Task MainAsync(
+    [Option("path")] string projectPath)
+{
+    await $"cd {projectPath}";
+
+    var projectSettingPath = Path.Combine(projectPath, "ProjectSettings", "TagManager.asset");
+
+    var tagManager = YamlSerializer.Deserialize<dynamic>(File.ReadAllBytes(projectSettingPath));
+
+    var layers = tagManager["TagManager"]["layers"];
+
+    log(layers);
+
+    var prefabFiles = Directory.GetFiles("./Assets", "*.prefab", SearchOption.AllDirectories);
+
+    foreach (var filePath in prefabFiles)
+    {
+        var file = File.ReadAllBytes(filePath);
+
+        var yaml = YamlSerializer.Deserialize<Dictionary<object, dynamic>>(file);
+
+        if (!yaml.ContainsKey("GameObject"))
+        {
+            continue;
+        }
+        
+        var gameObject = yaml["GameObject"];
+        
+        if(gameObject["m_TagString"] == "FindMe")
+        {  
+            log($"FindMe: {filePath}");
+        }
+        
+        if(layers[gameObject["m_Layer"]] == "LookAtMe")
+        {  
+            log($"LookAtMe: {filePath}");
+        }
+    }
+}
